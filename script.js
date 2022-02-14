7b87b6c74beae582999950e7ca1f9ae7196215d9
kd-rgb <92637769+kd-rgb@users.noreply.github.com>
2022-02-14 15:34:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (parseFloat(principal) * parseFloat(years) * parseFloat(rate)) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+	document.getElementById("result").innerHTML	= "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%,\<br\>you will receive an amount of $" + interest + "\<br\>in interest in the year " + year + "\<br\>";
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
